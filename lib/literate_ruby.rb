7441b7bfef41adfbc4a8f2193e242444ad671941
Andrew O'Brien <obrien.andrew@gmail.com>
2011-06-24 19:13:05 -0400

    Loading working; requiring working but removed for now.

--- lib/literate_ruby.rb
+++ lib/literate_ruby.rb
@@ -1,37 +1,124 @@
 module LiterateRuby
-  def self.require(path)
-    
-  end
+  # See comment in the test file
+  # def self.require(path)
+  #   full_path = path + ".lrb"
+  #   if $".include?(full_path)
+  #     false
+  #   else
+  #     if load(full_path)
+  #       $" << full_path
+  #     else
+  #       raise LoadError
+  #     end
+  #   end
+  # end
   
   def self.load(path)
-    
-  end
-  
-  def self.eval(string)
-    
-  end
-  
-  def self.parse(io)
-    state = :not_in_code_block
-    lines = []
+    lines = parse(File.new(path))
 
-    io.readlines.each do |line|
+    lines.find_all {|(type, line_group)| type == :code && !line_group.empty?}.each do |(_, line_group)|
+      Kernel.eval(
+        line_group.map {|(line_no, line)| line}.join(""), 
+        TOPLEVEL_BINDING, 
+        path, 
+        line_group[0][0]
+      )
+    end
+
+    lines
+  end
+
+  def self.parse(io)
+    state = :open
+    lines = []
+    buffer = nil
+    
+    io.readlines.each_with_index do |line, i|
       case line
       when /^>/
-        lines << [:code, line.sub(/^>/, "")]
-      when /^=begin/
+        clean_line = line.sub(/^>/, "")
+        case state
+        when :open
+          state = :in_code
+          buffer = []
+          buffer << [i + 1, clean_line]
+        when :in_code
+          buffer << [i + 1, clean_line]
+        when :in_code_block
+          buffer << [i + 1, clean_line]
+        when :in_data
+          lines << [:data, buffer]
+          buffer = []
+          
+          state = :in_code
+          buffer << [i + 1, clean_line]
+        end
+      when /^=begin_code/
+        case state
+        when :open
+          buffer = []
+        when :in_code
+          lines << [:code, buffer]
+          buffer = []
+        when :in_code_block
+          buffer << [i + 1, line]
+        when :in_data
+          lines << [:data, buffer]
+          buffer = []
+        end
         state = :in_code_block
-      when /^=end/
-        state = :not_in_code_block
+      when /^=end_code/
+        case state
+        when :open
+          buffer = []
+          state = :in_data
+        when :in_code
+          lines << [:code, buffer]
+          buffer = []
+          
+          state = :in_data
+        when :in_code_block
+          lines << [:code, buffer]
+          buffer = []
+          
+          state = :in_data
+        when :in_data
+          # no-op
+        end
       else
-        if state == :in_code_block
-          lines << [:code, line]
-        else
-          lines << [:data, line]
+        case state
+        when :open
+          buffer = []
+          state = :in_data
+          buffer << [i + 1, line]
+        when :in_code
+          lines << [:code, buffer]
+          buffer = []
+          
+          state = :in_data
+          buffer << [i + 1, line]
+        when :in_code_block
+          buffer << [i + 1, line]
+        when :in_data
+          buffer << [i + 1, line]
         end
       end
     end
-    
+
+    case state
+    when :open
+      # no-op
+    when :in_code
+      lines << [:code, buffer] unless buffer.empty?
+      buffer = nil
+    when :in_code_block
+      lines << [:code, buffer] unless buffer.empty?
+      buffer = nil
+    when :in_data
+      lines << [:data, buffer] unless buffer.empty?
+      buffer = nil
+    end
+
     lines
   end
 end
--- test/literate_ruby_test.rb
+++ test/literate_ruby_test.rb
@@ -15,23 +15,65 @@ require "literate_ruby"
 FIXTURE_DIR = File.join(File.dirname(__FILE__), "fixtures")
 
 describe LiterateRuby do
-  it "parses literal ruby files and returns code and data sections" do
+  it "parses literate ruby files returning annotated lines" do
     lines = LiterateRuby.parse(File.new(File.join(FIXTURE_DIR, "fixture.lrb")))
 
+    assert_fixture_parses(lines)
+  end
+  
+  it "loads a literate Ruby file returning annotated lines" do
+    lines = LiterateRuby.load(File.join(FIXTURE_DIR, "fixture.lrb"))
+
+    assert_fixture_parses(lines)
+
+    assert { FOO == 123 }
+    assert { foo() == 2 }
+    assert { BAR == 456 }
+  end
+
+  # Leaving this out for now. It works, but until I see someone trying to write libs in lrb, it might not be worth it to
+  # rewrite Ruby's load path searching, especially since that's changing.
+  # it "requires a literate Ruby file" do
+  #   LiterateRuby.require(File.join(FIXTURE_DIR, "require_fixture"))
+  # 
+  #   assert { bar() == 80 }
+  #   assert { BAZ == 678 }
+  #   assert { $".grep(/require_fixture.lrb$/) }
+  #   
+  #   deny { LiterateRuby.require(File.join(FIXTURE_DIR, "require_fixture")) }
+  # end
+
+  def assert_fixture_parses(lines)
     expected = [
-      [:data, %{FOO is declared to be 123\n}],
-      [:data, %{Let's test multines\n}],
-      [:data, "\n"],
-      [:data, %{And paragraph breaks\n}],
-      [:code, %{ FOO == 123\n}],
-      [:code, %{ def foo\n}],
-      [:code, %{  1 + 1\n}],
-      [:code, %{ end\n}],
-      [:data, "\n"],
-      [:data, "BAR is 456\n"],
-      [:data, "\n"],
-      [:code, "BAR = \n"],
-      [:code, "  456\n"]
+      [:data, 
+        [
+          [1, "FOO is declared to be 123\n"],
+          [2, "Let's test multines\n"],
+          [3, "\n"],
+          [4, "And paragraph breaks\n"]
+        ]
+      ],
+     [:code, 
+       [
+         [5, " FOO = 123\n"], 
+         [6, " def foo\n"], 
+         [7, "  1 + 1\n"], 
+         [8, " end\n"]
+        ]
+      ],
+      [:data, 
+        [
+          [9, "\n"], 
+          [10, "BAR is 456\n"], 
+          [11, "\n"]
+        ]
+      ],
+      [:code, 
+        [
+          [13, "BAR = \n"], 
+          [14, "  456\n"]
+        ]
+      ]
     ]
     
     assert { lines == expected}
